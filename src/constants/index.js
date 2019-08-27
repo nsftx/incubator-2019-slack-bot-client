f546875b3498352a7df41eb68b0385b2497bfe31
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2019-08-27 12:08:39 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 //urls
-export const API_BASE_URL = 'http://localhost:8080';
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const API_BASE_URL = 'http://welcome-bot.tk:9090';
+export const OAUTH2_REDIRECT_URI = 'http://welcome-bot.tk:80/oauth2/redirect'
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 //User things
 export const ACCESS_TOKEN = 'accessToken';
